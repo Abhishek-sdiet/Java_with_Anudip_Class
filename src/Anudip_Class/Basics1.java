442858271b5ca081ebb786646ccab1e57fd198a0
Abhishek <abhi20040805@gmail.com>
2026-08-11 15:44:06 +0530

    Creating repo and adding some of work completed til

---
+++ src/Anudip_Class/Basics1.java
@@ -0,0 +1,57 @@
+package Anudip_Class;
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Basics1 {
+    public static void main(String args[]){
+       Scanner sc = new Scanner(System.in);
+
+
+        // Bonus calculator
+
+//        double salary = 38500;
+//        double per = 12.5;
+//
+//        double bonus = (salary/100)*per;
+//        System.out.println("Bonus of your salary : " + bonus);
+
+
+        // Login System
+//
+//        String UserId = "Abhi";
+//        String password = "abhi123";
+//        System.out.println("Welcome to Anudip Classes");
+//        System.out.println("Please Enter your User Id");
+//        String Id = sc.nextLine();
+//        if(Id.equalsIgnoreCase(UserId)){
+//            System.out.println("Please enter your password : ");
+//            String pass = sc.nextLine();
+//            if(pass.equals(password)){
+//                System.out.println("Login Successful");
+//            }else{
+//                System.out.println("Wrong password , please enter correct password");
+//            }
+//        }else {
+//            System.out.println("User Id not found , Please enter correct User Id");
+//        }
+
+
+        // Type Conversion
+
+//        System.out.println("Enter the Character : ");
+//        char ch = sc.next().charAt(0);
+//        int chVal = ch;
+//        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+//            if(chVal=>65 && chVal=<97){
+//                ch = (char)chVal;
+//                System.out.println("Entered Character is a Vowel : " );
+//            }
+//            else{
+//                System.out.println();
+//            }
+//        }
+
+
+
+    }
+}
---
+++ src/Anudip_Class/Home_Work/Aug10.java
@@ -0,0 +1,87 @@
+package Anudip_Class.Home_Work;
+
+import java.util.Scanner;
+
+public class Aug10 {
+
+    // ATM Simulation System
+
+    static void main() {
+        Scanner sc = new Scanner(System.in);
+//        int pin = 1234;
+//        int avlBalance=20000;
+//        System.out.println("    Welcome to the ATM      ");
+//        System.out.println("----------------------------");
+//        System.out.println("Type 1 for Saving and 2 for Current account");
+//        System.out.println("1:- Saving  ");
+//        System.out.println("2:- Current  ");
+//        System.out.println();
+//        int accType = sc.nextInt();
+//
+//        if(accType == 1){
+//            System.out.println("Please enter your PIN : ");
+//            int pinByUser = sc.nextInt();
+//            if(pin == pinByUser){
+//                System.out.println("Enter the amount of money : ");
+//                int amount = sc.nextInt();
+//                if(amount >=10000){
+//                    System.out.println("please enter amount not more than 10,000");
+//                }else if(amount == 0){
+//                    System.out.println("please enter amount Greater than 0");
+//                }else if(amount >= avlBalance){
+//                    System.out.println("Amount entered is greater than available balance");
+//                }else{
+//                    System.out.println("Please collect you money : " + amount);
+//                    System.out.println("Your Available balance is : " + (avlBalance - amount));
+//                }
+//            }else{
+//                System.out.println("Please enter the correct PIN");
+//            }
+//
+//        }else{
+//            System.out.println("Sorry you don't have a Current account");
+//        }
+
+        // Car Parking Fees
+
+//        System.out.println("Welcome to Parking ");
+//        System.out.println("Please enter the no of hours you parked your Car :  ");
+//        int hours = sc.nextInt();
+//        if(hours > 0 && hours <= 2){
+//            System.out.println("Your Car parking fee is : 30");
+//        }else if(hours >=3  && hours <=5){
+//            System.out.println("Your Car parking fee is : 50");
+//        }else if(hours >=6 && hours <= 10){
+//            System.out.println("Your Car parking fee is : 80");
+//        }else{
+//            System.out.println("Your Car parking fee is : 120");
+//        }
+
+
+        // Recharge Discount
+
+//        System.out.println("Enter the amount of Recharge : ");
+//        double discount ;
+//        int finalAmt ;
+//        int rechargeAmt = sc.nextInt();
+//        if(rechargeAmt <200){
+//            System.out.println("Recharge done , sorry discount available above 200 only");
+//        }else if(rechargeAmt>=200 && rechargeAmt<500){
+//            System.out.println("Recharge amount : " + rechargeAmt);
+//            System.out.println("Discount applicable : " + (rechargeAmt*0.05));
+//            System.out.println("Final payable amount is : " + (rechargeAmt - rechargeAmt*0.05));
+//        }else if(rechargeAmt>=500 && rechargeAmt<1000){
+//            System.out.println("Recharge amount : " + rechargeAmt);
+//            System.out.println("Discount applicable : " + (rechargeAmt*0.10));
+//            System.out.println("Final payable amount is : " + (rechargeAmt - rechargeAmt*0.10));
+//        }else if(rechargeAmt>=1000){
+//            System.out.println("Recharge amount : " + rechargeAmt);
+//            System.out.println("Discount applicable : " + (rechargeAmt*0.15));
+//            System.out.println("Final payable amount is : " + (rechargeAmt - rechargeAmt*0.15));
+//        }
+
+        //
+    }
+
+
+}
